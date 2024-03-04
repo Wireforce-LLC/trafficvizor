14b493a01c019dfb049f0dbe92a108287c7f6f89
Maslow Vladislav <70482199+masloffvs@users.noreply.github.com>
2024-03-04 03:18:02 +0300

    temp: ignoreBuildErrors

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,12 @@ const nextConfig = {
    */
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  // !! WARN !!
+  // Dangerously allow production builds to successfully complete even if
+  // your project has type errors.
+  // !! WARN !!
+  ignoreBuildErrors: true,
 };
 
 export default nextConfig;
